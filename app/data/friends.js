21e2743184a8c499cd35b415cfe787b8d44a6a5b
Hassan Ali <Dotmatrix_fusion@hotmail.com>
2018-02-08 01:10:34 -0800

    adds complete file

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friends = [
+
+	{
+		name:"John Cena",
+		photo: "https://shop.wwe.com/on/demandware.static/-/Sites-wwe-us-navigation/default/dw2a6de264/images/superstar-thumb-300/john-cena-2017.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"4",
+			"5",
+			"1",
+			"2",
+			"5",
+			"4",
+			"1"
+		]
+	},
+	{
+		name:"This Guy From Spongebob",
+		photo: "https://vignette.wikia.nocookie.net/spongebob/images/1/1f/SandySandals.png/revision/latest?cb=20160723200438",
+		scores: [
+			"3",
+			"2",
+			"4",
+			"1",
+			"2",
+			"4",
+			"5",
+			"2",
+			"1",
+			"5"
+		]
+	},
+	{
+		name:"Ronaldo",
+		photo: "http://e2.365dm.com/18/02/16-9/20/cristiano-ronaldo-real-madrid-la-liga_4223412.jpg?20180205154613",
+		scores: [
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"5",
+			"4",
+			"2",
+			"2",
+			"4"
+		]
+	},
+	{
+		name:"Messi",
+		photo: "https://www.thesun.co.uk/wp-content/uploads/2017/05/nintchdbpict000327373302-e1495923278521.jpg?strip=all&w=960",
+		scores: [
+			"1",
+			"4",
+			"1",
+			"3",
+			"1",
+			"5",
+			"4",
+			"2",
+			"2",
+			"4"
+		]
+	}
+
+]
+
+module.exports = friends;
